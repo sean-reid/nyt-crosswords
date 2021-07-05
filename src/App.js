63f227810a010c1d14a91f6e4b27b598e05ae02b
sean reid <seanreid.mail@gmail.com>
2021-07-05 22:52:20 +0000

    use more default vals in getData

--- src/App.js
+++ src/App.js
@@ -3,18 +3,8 @@ import './App.css';
 
 // Example GET method implementation:
 async function getData(url = '') {
-  // Default options are marked with *
   const response = await fetch(url, {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    method: 'GET',
   });
   return response.json(); // parses JSON response into native JavaScript objects
 }
