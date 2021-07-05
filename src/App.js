edd2dec62f8dd180fbdb53f05e6c17a752e218e4
sean reid <seanreid.mail@gmail.com>
2021-07-05 22:43:49 +0000

    getData, not postData

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 // Example GET method implementation:
-async function getData(url = '', data = {}) {
+async function getData(url = '') {
   // Default options are marked with *
   const response = await fetch(url, {
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
@@ -15,13 +15,12 @@ async function getData(url = '', data = {}) {
     },
     redirect: 'follow', // manual, *follow, error
     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
   });
   return response.json(); // parses JSON response into native JavaScript objects
 }
 
 function App() {
-  postData('https://raw.githubusercontent.com/sean-reid/nyt_crosswords/master/2016/05/12.json', {})
+  getData('https://raw.githubusercontent.com/sean-reid/nyt_crosswords/master/2016/05/12.json')
   .then(data => {
     console.log(data); // JSON data parsed by `data.json()` call
   });
