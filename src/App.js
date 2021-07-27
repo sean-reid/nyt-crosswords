2c08990b7926383154a98539144b10aed2fbfe1e
sean reid <seanreid.mail@gmail.com>
2021-07-27 22:35:27 +0000

    some changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import Crossword from '@jaredreisinger/react-crossword';
+
+Number.prototype.pad = function(size) {
+  var s = String(this);
+  while (s.length < (size || 2)) {s = "0" + s;}
+  return s;
+}
+
+function status(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return Promise.resolve(response)
+  } else {
+    return Promise.reject(new Error(response.statusText))
+  }
+}
+
+function json(response) {
+  return response.json()
+}
+
+
+
+function ConvertData(data) {
+
+}
 
 function App() {
+  const year = 2011;
+  const month = 2;
+  const day = 20;
+  let url = `https://raw.githubusercontent.com/sean-reid/nyt-crosswords-data/master/${(year).pad(4)}/${(month).pad(2)}/${(day).pad(2)}.json`
+  fetch(url)
+  .then(status)
+  .then(json)
+  .then(function(data) {
+    console.log('Request succeeded with JSON response', data);
+    return data
+  }).then(ConvertData)
+  .catch(function(error) {
+    console.log('Request failed', error);
+  });
   return (
     <div className="App">
       <header className="App-header">
