1a2904749722c9001b1646f90eadfd4be974677a
sean reid <seanreid.mail@gmail.com>
2021-07-27 23:35:06 +0000

    other changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Crossword from '@jaredreisinger/react-crossword';
 
+//https://stackoverflow.com/questions/16102263/to-find-index-of-multidimensional-array-in-javascript
+function getIndexOfK(arr, k) {
+  for (var i = 0; i < arr.length; i++) {
+    var index = arr[i].indexOf(k);
+    if (index > -1) {
+      return [i, index];
+    }
+  }
+}
+
 Number.prototype.pad = function(size) {
   var s = String(this);
   while (s.length < (size || 2)) {s = "0" + s;}
@@ -23,7 +33,63 @@ function json(response) {
 
 
 function ConvertData(data) {
+  let size = data.size;
+  let answers = data.grid;
+  let nums = data.gridnums;
+  let answersGrid = [];
+  let answersRow = [];
+  for (let index_rows = 0; index_rows < size.rows; index_rows++) {
+    answersRow = [];
+    for (let index_cols = 0; index_cols < size.cols; index_cols++) {
+      answersRow.push(answers[size.cols*index_rows + index_cols]);
+    }
+    answersGrid.push(answersRow);
+  }
+  let numsGrid = [];
+  let numsRow = [];
+  for (let index_rows = 0; index_rows < size.rows; index_rows++) {
+    numsRow = [];
+    for (let index_cols = 0; index_cols < size.cols; index_cols++) {
+      numsRow.push(nums[size.cols*index_rows + index_cols]);
+    }
+    numsGrid.push(numsRow);
+  }
+  let acrossClues = data.clues.across;
+  let downClues = data.clues.down;
+  let clueNum = 0;
+  let result = [];
+  let answer = "";
+  let clue = "";
+  for (let index_across = 0; index_across < acrossClues.length; index_across++) {
+    clue = acrossClues[index_across]
+    clueNum = GetClueNumber(clue);
+    result = getIndexOfK(numsGrid, clueNum);
+    answer = answersGrid[result[0]][result[1]];
+    answer = GetAnswer(answersGrid, result, "across");
+    console.log(clue, answer);
+  }
+}
 
+function GetAnswer(answersGrid, indicies, direction) {
+  let answer = "";
+  let row = indicies[0];
+  let col = indicies[1];
+  let current_letter = answersGrid[row][col];
+  while (current_letter !== "*") {
+    answer += current_letter;
+    if (direction === "across") {
+      col++;
+    } else if (direction === "down") {
+      row++;
+    }
+    current_letter = answersGrid[row][col];
+  }
+  return answer;
+}
+
+function GetClueNumber(clue) {
+  let clueNum = parseInt(clue.split('.')[0]);
+  return clueNum
 }
 
 function App() {
